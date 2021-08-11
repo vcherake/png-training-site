a1a08037eb8d79763b97976dee3ff37a8e431a14
vcherake <cherakeval@gmail.com>
2021-08-11 14:25:03 +1000

    public path

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     // filename: "[name].[contenthash].js",
-    publicPath: "/",
+    publicPath: "/png-training-site",
   },
   module: {
     rules: [
